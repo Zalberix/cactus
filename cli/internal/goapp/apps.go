2c71cda1986629c60a61aa23953ae68e96bb8d7b
Zalberix <zalberix@gmail.com>
2026-03-26 10:10:41 +0500

    Обновлен конфиг workers

--- cli/internal/goapp/apps.go
+++ cli/internal/goapp/apps.go
@@ -11,6 +11,6 @@ type App struct {
 // Apps is the canonical list of Go services in the monorepo.
 var Apps = []GoApp{
 	{Name: "core", DebugPort: 2346, ConfigFileName: "configs/apps/core.yaml", AppDir: "core"},
-	//{Name: "telegram", DebugPort: 2347},
-	//{Name: "smtp", DebugPort: 2348},
+	//{Name: "telegram", DebugPort: 2347, ConfigFileName: "configs/apps/workers/telegram.yaml", AppDir: "workers/telegram"},
+	//{Name: "smtp", DebugPort: 2348, ConfigFileName: "configs/apps/workers/smtp.yaml", AppDir: "workers/smtp"},
 }
